ee3c344cc9cf2d04cb098cf98545021b2d99ab2c
LucasChambi <lucasbrunocc880@outlook.com>
2025-11-03 20:10:06 -0300

    ajusta o repo

--- portfolio/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Lucas Bruno Calle Chambi</h1>
-    </div>    
-  );
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center font-sans text-red-500">Lucas Bruno Calle Chambi</h1>
+      <p className="text-center">Meu primeiro portfolio</p>
+    </div>    
+  );
+}
